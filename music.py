61cfbf71db5262a4ca617deaf57ecb770948c9fc
Adam Boult <adamdboult@gmail.com>
2016-06-29 19:08:35 +0100

    Fixed folder issues

--- music.py
+++ music.py
@@ -16,7 +16,8 @@ import subprocess
 ########
 from myConf import *
 
-dir = os.path.dirname(__file__)
+dir = os.path.dirname(os.path.realpath(__file__))
+#dir = os.path.dirname(__file__)
 ##########################
 # Get list of done files #
 ##########################
@@ -69,24 +70,32 @@ def unzipMusic(fileObj, prePathLocal):
 def folderCheck(prePathLocal, fileObj):
     filename, file_extension = os.path.splitext(fileObj)
     folderPath = os.path.dirname(prePathLocal + filename + "/")
+    print ("making: "+folderPath)
     ### Local folders
     if not os.path.exists(folderPath):
+        print ("a")
         os.makedirs(folderPath)
 
 #############
 # Copy file #
 #############
 def copyFile(fileObj, prePathLocal):
+    print (sourceFolder)
     filename, file_extension = os.path.splitext(fileObj)
     folderPath = os.path.dirname(prePathLocal + filename + "/")
     filePath = folderPath + "/" + os.path.basename(folderPath) + file_extension
+    print (sourceFolder)
     os.chdir(folderPath)
-    remotePath = sourceFolder + filePath
+    print (sourceFolder)
+    remotePath = sourceFolder + fileObj
+    print ("RMP: "+remotePath)
+    print ("sou: "+sourceFolder)
+    print ("PAT:"+ filePath)
     remotePath = remotePath.replace(" ", "\ ").replace("(", "\(").replace(")","\)").replace("|", "\|")
     filePath=filePath.replace(" ", "\ ").replace("(", "\(").replace(")","\)").replace("|", "\|")
     systemScript = 'rsync -avz '+HOST+'":'+remotePath + '" ' + filePath
     print (systemScript)
-    #os.system(systemScript)
+    os.system(systemScript)
 
 ##################
 # Convert to MP3 #
@@ -98,19 +107,15 @@ def toMP3(fileObj, prePathLocal, bitRate):
     os.chdir(folderPath)
     print ("IN: "+folderPath)
     toMp3Path = os.path.join(dir, 'tomp3.sh')
+    print ("here" + toMp3Path)
     subprocess.call(toMp3Path)
+    print ("there")
     systemScript = 'find . -name "*.flac" -type f -delete; find . -name "*.gz" -type f -delete; find . -name "*.zip" -type f -delete; find . -name "*.tar" -type f -delete'
     os.system(systemScript)
     
-    systemScript = 'mv ' + prePathLocal + '**/* ' + destinationFolder
+    systemScript = 'rsync -a ' + prePathLocal + '* ' + destinationFolder
+    print (systemScript)
     os.system(systemScript)
-
-##############
-# Update log #
-##############
-def updateLog(recordFile, fileObj):
-    with open(recordFile, "a") as myfile:
-        myfile.write(fileObj + "\n")
 
 #####################
 # Flatten directory #
@@ -121,6 +126,7 @@ def flattenDir(fileObj, prePathLocal):
     filePath = folderPath + "/" + os.path.basename(folderPath) + file_extension
     systemScript = 'find ./ -mindepth 2 -type f -exec mv -i "{}" ./ ";"'#;find -mindepth 1 -maxdepth 1 -type d -exec rm -r {} \;'
     os.chdir(folderPath)
+    print (folderPath)
     os.system(systemScript)
     systemScript = 'rm -r */'
     os.chdir(folderPath)
@@ -148,29 +154,26 @@ for fileEntry in fileListPre:
 i = 0
 bitRate = 256
 #print ("T")
-#print (musicList)
+print (musicList)
 for fileObj in fileList:
     filename, file_extension = os.path.splitext(fileObj)
-    print (filename)
+    #print (filename)
     if (filename in musicList):
         continue
 
     print ("Starting: " + fileObj)
     
-    #print ("Folder Check")
-    #folderCheck(prePathLocal, fileObj)
+    print ("Folder Check")
+    folderCheck(prePathLocal, fileObj)
 
-    #print ("Copy file")
-    #copyFile(fileObj, prePathLocal)
+    print ("Copy file")
+    copyFile(fileObj, prePathLocal)
 
-    #print ("Unzip file")
-    #unzipMusic(fileObj, prePathLocal)
+    print ("Unzip file")
+    unzipMusic(fileObj, prePathLocal)
 
-    #print ("Flatten directory")
-    #flattenDir(fileObj, prePathLocal)
+    print ("Flatten directory")
+    flattenDir(fileObj, prePathLocal)
     
-    #print ("Convert files")
-    #toMP3(fileObj, prePathLocal, bitRate)
-
-    #updateLog(recordFile, fileObj)
-    #print ("Done")
+    print ("Convert files")
+    toMP3(fileObj, prePathLocal, bitRate)
